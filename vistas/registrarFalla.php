8a106848355c13e56a1a1175f67888534ed8ce0c
Maria Guadalupe Villalobos Rodriguez <125302410+mariivr@users.noreply.github.com>
2023-02-20 10:11:35 -0600

    Avance vista registrar falla

---
+++ vistas/registrarFalla.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrar falla</title>
+</head>
+
+<header>
+    <h1>Fallucapp</h1>
+</header>
+
+<body>
+    <h2>Registrar falla</h2>
+    
+<form action="">
+    <p>
+        Código
+        <input type="text" name="codigo">
+    </p>
+
+    <p>
+        Nombre
+        <select name="nombre" id="nombre">
+
+        </select>
+    </p>
+
+    <p>
+        Ubicación
+        <input type="text" name="ubicacion">
+    </p>
+
+    <p>
+        Descripción
+        <textarea name="descripcion"></textarea>
+    </p>
+
+    <p>
+
+         Dispositivo
+      
+        <select name="dispositivo" id="dispositivo">
+      
+          <option>Seleccionar</option>
+      
+          <option>Router</option>
+      
+          <option>Switch</option>
+      
+        </select>
+      
+      </p>
+
+      <p><input type="submit" value="Registrar"></p>
+      
+
+</form>
+    
+</body>
+
+
+</html>
