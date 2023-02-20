9ca3899aa680070d00d4a50d4b0bfa751e6ea5ae
Eli777z <125302631+Eli777z@users.noreply.github.com>
2023-02-20 17:24:21 +0100

    Ventana para ver los dispositivos

---
+++ vistas/dispositivos.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dispositivos</title>
+</head>
+<header>
+    <nav>
+        <h3 style="color: rgb(243, 222, 202);">Fallucapp</h3>
+        <a href="" style="color: azure;">Regresar</a>
+
+    </nav>
+</header>
+
+
+<body>
+   <div id="bloqueDispositivos" name="bloqueDispositivos">
+    <h3>Dispositivos</h3>
+        <div id="btnAgregarDispositivos" name="btnAgregarDispositivos">
+            <button><img src="img/agregar.png" alt=""></button>
+        </div>
+        <div id="cajaBuscar" name="cajaBuscar">
+            <input type="text">
+        </div>
+        <div id="btnBuscarDispositivos" name="btnBuscarDispositivos">
+            <button><img src="img/buscar.png" alt=""></button>
+        </div>
+   </div> 
+   <section id="tablaDispositivos" name="ta
+   ">
+        <table id="tDispositivos" name="tDispositivos" border="">
+            <tr>
+                <td>
+                    <p>Nombre</p>
+                </td>
+                <td>
+                    <p>Tipo</p>
+                </td>
+                <td>
+                    <p>Descripción</p>
+                </td>
+                <td>
+                    <p>Ubicación</p>
+                </td>
+                <td>
+                    <p>Configuración</p>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+
+        </table>
+
+   </section>
+
+
+   </div>
+</body>
+</html>
