5fdd5a2ae6b409c30479abbde857966bcbdcace3
MaToAng <125302640+MaToAng@users.noreply.github.com>
2023-02-20 17:17:46 +0100

    Ventana para registrar dispositivo

---
+++ vistas/registrarDispositivo.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrar dispositivo</title>
+</head>
+<body>
+    <section class="form-register">
+        <h2>Registrar Dispositivo</h2>
+        <label>Nombre:</label>
+        <input type="text" name="nombre" id="nombre" placeholder="Nombre del dispositivo">
+        <label>Tipo:</label>
+        <input type="text" name="tipo" id="tipo" placeholder="Tipo de dispositivo">
+        <label>Ubicación:</label>
+        <input type="text" name="ubicacion" id="ubicacion" placeholder="Ubicación del dispositivo">
+        <label>Descripción:</label>
+        <textarea name="descripcion" id="descripcion" placeholder="Descripción"></textarea>
+        <label>Configuración:</label>
+        <input type="text" name="configuracion" id="configuracion" placeholder="Configuración">
+        <p><input type="submit" value="Registrar"></p>
+        <p><input type="submit" value="Agregar"></p>
+    </section>
+</body>
+</html>
