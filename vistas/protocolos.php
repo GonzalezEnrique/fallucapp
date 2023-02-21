8c9adc2edf8382c5b68948ba84d6223717282b0c
jacquipc <125302461+jacquipc@users.noreply.github.com>
2023-02-21 11:55:59 -0600

    ventana protocolos

---
+++ vistas/protocolos.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Protocolos</title>
+    <link rel="stylesheet" type="text/css" href="../recursos/css/mystyle.css">
+</head>
+
+<header>
+    <h1>Protocolos</h1>
+</header>
+
+<body>
+
+    <div id="casa" name="casa">
+        <a href="contenido/fallucap.php" target="_parent" > <h3 id="inicioA" name="inicioA">Inicio</h3> </a>
+    </div>
+        
+    <div id="botonSubir" name="botonSubir">
+            <label for="file-input">
+                <img src="../recursos/imagenes/subirA.jpg" height="87px" width="87px">
+            </label>
+            <input id="file-input" type="file" style="display: none;">
+    </div>
+
+
+        <div id="botonEliminar" name="botonEliminar">
+            <button type="button" id="botonEliminar" name="botonEliminar">
+                <img src="../recursos/imagenes/eliminarA.jpg" height="87px" width="87px">
+            </button>
+        </div>
+
+        <div id="lista" name="lista">
+            <ol>
+                <li>Protocolo tipo 1</li>
+                <li>Protocolo tipo 2</li>
+                <li>Protocolo tipo 3</li>
+            </ol>
+        </div>
+</body>
+</html>
