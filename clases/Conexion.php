53fd2e00808652ba4476789fe04cae9d53775d96
GabrielCiprianoLara <cilg011022@itsuruapan.edu.mx>
2023-02-21 12:45:25 -0600

    Se agrego la clase para la conexion a la bd

---
+++ clases/Conexion.php
@@ -0,0 +1,32 @@
+<?php
+class ConexionBD {
+    private $host = "nombre_del_servidor"; 
+    private $usuario = "nombre_de_usuario"; 
+    private $contrasena = "contraseña"; 
+    private $nombre_bd = "nombre_de_la_base_de_datos";
+    private $conexion;
+
+    //método para conectar a la base de datos
+    public function conectar() {
+        $dsn = "mysql:host=$this->host;dbname=$this->nombre_bd;charset=utf8mb4";
+        $opciones = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        $this->conexion = new PDO($dsn, $this->usuario, $this->contrasena, $opciones);
+    }
+
+    //método para cerrar la conexión a la base de datos
+    public function desconectar() {
+        $this->conexion = null;
+    }
+
+    //método para ejecutar una consulta en la base de datos
+    public function ejecutarConsulta($consulta, $parametros = []) {
+        $stmt = $this->conexion->prepare($consulta);
+        $stmt->execute($parametros);
+        $resultado = $stmt->fetchAll();
+        return $resultado;
+    }
+}
