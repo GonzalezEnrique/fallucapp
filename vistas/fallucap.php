eab6008eff7db47ca565720a8f161f3abd75ae35
AndreaMendezCaracoza <125303406+AndreaMendezCaracoza@users.noreply.github.com>
2023-02-21 13:24:46 -0600

    Update fallucap.php

--- vistas/fallucap.php
+++ vistas/fallucap.php
@@ -5,54 +5,74 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fallucapp</title>
+<style>
+    .cuadro {
+        background-color: #2d4057;
+        padding: 15px;
+        text-align: center;
+    
+    }
+    .letra{
+        color: #fa9a83;
+    }
+        
+    
+</style>
+
 </head>
 <header>
     
-    
+    <div class="cuadro">
+        
     <h2> Fallucapp</h2>
-   
+</div>
 
         <body>
             <section id="pantalla" name="pantalla">
              <table id="tpantalla" name="tpantalla" border="1">
                 <tr>
+                  
         <h3>Buscar</h3>
-        <div>
         
-            <img src="/recursos/imagenes/lupa.jpg" width="80" height="80">
+        <div>
+           
+            <img src="Imagenes/lupa.jpg" width="80" height="80">
             
         </div>
         <tr>
             <td>
-        <a href="Fallas.php"><h3>Fallas</h3></a>
+                
+        <a href="Contenido/Fallas.php"> <h3>Fallas</h3></a>
         <div>
             
-            <img src="/recursos/imagenes/fallas.jpg" width="80" height="80 ">
+            <img src="Imagenes/fallas.jpg" width="80" height="80 ">
         </div>
+    </div>
     </td>
     <td>
-        <a href="dispositivos.php"><h3>Dispositivos</h3></a>
+        
+        <a href="Contenido/Dispositivos.php"><div class="cuadro2"><h3>Dispositivos</h3></a>
         <div>
         
-            <img src="/recursos/imagenes/dispositivo.jpg" width="80" height="80 ">
+            <img src="Imagenes/dispositivo.jpg" width="80" height="80 ">
         </div>
+    </div> 
     </td>
     <td>
-        <a href="protocolos.php"><h3>Protocolos</h3></a>
+        <a href="Contenido/Protocolos.php"><div class="cuadro2"><h3>Protocolos</h3></a>
+       
         <div>
         
-            <img src="/recursos/imagenes/protocolo.png" width="80" height="80 ">
+            <img src="Imagenes/protocolo.png" width="80" height="80 ">
         </div>
+       
+        
     </nav>
 </header>
 </td>
-</tr>
-<tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-</tr>
+
+   
+
 </table>
 </section>   
 </body>
