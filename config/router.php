6cd08eed5afd86b88fd9a747cef4287e362357ac
GonzalezEnrique <goae011205@itsuruapan.edu.mx>
2023-02-20 19:11:06 -0600

    Configuración

--- config/router.php
+++ config/router.php
@@ -1,14 +1,16 @@
 <?php
 
+
+
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 echo $uri;
 
 $routes = [
-    '/fallucapp/' => 'vistas/fallucap.php',
-    '/fallucapp/fallas/' => 'vistas/Fallas.php',
+    '/' => 'vistas/fallucap.php',
+    '/fallas' => 'vistas/Fallas.php',
     '/fallucapp/registrar-falla' => 'vistas/registrarFalla.php',
     '/fallucapp/protocolos' => '../vistas/protocolos.php',
-    '/fallucapp/dispositivos' => '../vistas/dispositivos.php',
+    '/dispositivos' => 'vistas/dispositivos.php',
     '/fallucapp/registrar-dispositvo' => '../vistas/registrarDispositivo.php'
 ];
 
@@ -16,8 +18,6 @@ if (array_key_exists($uri, $routes)) {
     require $routes[$uri];
 } else {
     //http_response_code(404);
-
     //require 'views/404.php';
-
     //die();
 }
--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
 require 'vistas/fallucap.php';
-//$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-//echo $_SERVER['REQUEST_URI'];
 
 ?>
