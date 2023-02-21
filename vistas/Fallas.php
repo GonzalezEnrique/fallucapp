0fc0e6990139d4bdeecf9af79909946e66929b58
AndreaMendezCaracoza <125303406+AndreaMendezCaracoza@users.noreply.github.com>
2023-02-21 13:23:53 -0600

    Update Fallas.php

--- vistas/Fallas.php
+++ vistas/Fallas.php
@@ -4,28 +4,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
     <title>Fallucapp</title>
+
+   
     <style>
+        .cuadro {
+        background-color: #2d4057;
+        padding: 15px;
+        text-align: center;
+    }
+    .letra{
+        color: #fa9a83;
+    }
         
         
     </style>
 </head>
 <header>
-   
-    <a href="fallucap.php">
-        <img src="/recursos/imagenes/menu.png" width="40" height="40" align="left">Fallas
+    
+    <a href="../index.php">
+        <div class="letra">
+            <div class="cuadro">
+                <div class="letra">
+            <h2> FALLAS</h2>
+        <img src="../Imagenes/menu.png" width="40" height="40" align="left"> 
         
-   
+    </div>
     </a>
     <div>
         <img src="/recursos/imagenes/lupa.jpg"width="50" height="50" align="right">
         <img src="/recursos/imagenes/mas.png"width="50" height="50" align="right">
-        
-        
     </div>
+</div>
+
+
+   
 </header>
 <body>
     
 </body>
 </html>
+
 
