a0ea87ca4bfefd03664cadf7062b577956357b8b
GonzalezEnrique <goae011205@itsuruapan.edu.mx>
2023-02-23 19:22:16 -0600

    Creación del método get_all_inventario() para obtener todos los datos de la tabla 'inventarios' en la BD

--- clases/Inventario.php
+++ clases/Inventario.php
@@ -1,5 +1,7 @@
 <?php
 
+include 'Conexion.php';
+
 class Inventario {
 
     private $id;
@@ -20,5 +22,28 @@ class Inventario {
         return $this->id_dispositivo;
     }
 
-    public function get_all_inventario() {}
+    public function get_all_inventario() {
+        $conn = new ConexionBD();
+        $sql = 'SELECT * FROM inventario INNER JOIN tipos_dispositivos ON inventario.idTipoDisp = tipos_dispositivos.idTipoDisp';
+        $conn->conectar();
+        
+        $datos = $conn->ejecutarConsulta($sql);
+
+        // Visualizar datos (forma 1)
+        echo "<pre>";
+        var_dump($datos);
+        echo "</pre>";
+
+        // Visualizar datos (forma 2)
+        foreach ($datos as $dato) {
+            echo "id: " . $dato["id"] . "<br>";
+            echo "cantidad: " . $dato["cantidad"] . "<br>";
+            //echo "idTipoDisp: " . $dato["idTipoDisp"] . "<br>";
+            echo "TipoDisp: " . $dato["tipo"] . "<br><hr>";
+        }
+    }
 }
+
+// Valores sin importancia en el contructor, solo para probar la consulta
+$inv = new Inventario(1, 2, 3);
+$inv->get_all_inventario();
