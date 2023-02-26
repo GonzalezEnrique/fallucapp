8d47fba586e52e41eff3024e9e0891f03215c424
AndreaMendezCaracoza <125303406+AndreaMendezCaracoza@users.noreply.github.com>
2023-02-25 21:45:55 -0600

    Add files via upload

---
+++ vistas/Fallas.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fallas</title>
+    <link rel="stylesheet" href="hojaAndy.css">
+
+</head>
+
+<header >
+    <div class="cuadro">
+    <nav class="classnav">
+        <a href="index.html">
+    
+            <img id="prinicpal" name="prinicpal" src= "imagenes/casita.png"> </a>
+            <nav class="letra">
+                
+            <h2>Fallucapp</h2>
+        </nav>
+
+<section id="secFormulario" name="secFormulario">
+      <tr>
+            <td><label for=""></label></td>
+            <img src="imagenes/Mas.png " width="50" height="50">
+            <img src="imagenes/lupa.png " width="50" height="50">
+            <td><input type="text" name="txnombre" id="txnombre"></td>
+      
+
+    </section>
+</div>
+<div class="cuadro2">
+    <nav class="letra2">
+    <nav class="classnav">
+        
+
+
+</nav>
+</nav>
+</div>
+    <body>
+        
+    </body>
+
+    </html>
