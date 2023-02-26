f7718b26cd109f2da63d80cc16b481ac39a08da3
AndreaMendezCaracoza <125303406+AndreaMendezCaracoza@users.noreply.github.com>
2023-02-25 18:57:26 -0600

    Add files via upload

---
+++ vistas/Fallucapp.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+    <title>Fallucapp</title>
+    <link rel="stylesheet" href="hojaAndy.css">
+
+</head>
+
+<header >
+    <div class="cuadro">
+    <nav class="classnav">
+        <a href="index.html">
+    
+            <img id="prinicpal" name="prinicpal" src= "imagenes/casita.png"> </a>
+            <nav class="letra">
+                
+            <h2>Fallucapp</h2>
+        </nav>
+
+<section id="secFormulario" name="secFormulario">
+      <tr>
+            <td><label for=""></label></td>
+            <img src="imagenes/lupa.png " width="50" height="50">
+            <td><input type="text" name="txnombre" id="txnombre"></td>
+      
+
+    </section>
+</div>
+<div class="cuadro2">
+    <nav class="letra2">
+    <nav class="classnav">
+        
+<a href="Fallas.html">Fallas</a>
+<img src="imagenes/falla.png" width="100" height="100">
+<a href="Dispositivos.html">Dispositivos</a>
+<img src="imagenes/cableEthernet.png" width="120" height="120">
+<a href="Protocolos.html">Protocolos</a>
+<img src="imagenes/carpetaa.png" width="150" height="150">
+
+</nav>
+</nav>
+</div>
+    <body>
+        
+    </body>
+
+    </html>
