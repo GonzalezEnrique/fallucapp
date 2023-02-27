93e748203814676cf8f7726d696109f2ed3b4f61
MaToAng <125302640+MaToAng@users.noreply.github.com>
2023-02-27 18:53:52 +0100

    Add files via upload

---
+++ vistas/registrarDispositivo.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../recursos/css/style.css">
+    <link rel="shortcut icon" href="../recursos/imagenes/logo.png" type="image/x-icon">
+    
+    <title>Registrar dispositivo</title>
+</head>
+<body>
+    <header>
+        <a href="../vistas/Fallucapp.php">
+            <div class="logo">
+                <img src="../recursos/imagenes/logo.png" width="90">
+            </div>
+        </a>
+        <h1>Fallucapp</h2>
+    </header>
+
+    <section class="form-register">
+        <form class="form">
+            <h2>Registrar dispositivo</h2>
+            <div class="form_container">
+                <div class="fg">
+                    <label class="form_label" for="nombre">Nombre:</label>
+                    <input class="controls" type="text" name="nombre" id="nombre" placeholder="Nombre del dispositivo">
+                </div>
+                <div class="fg">  
+                    <label class="form_label" for="tipo">Tipo:</label>
+                    <input class="controls" type="text" name="tipo" id="tipo" placeholder="Tipo de dispositivo">
+                </div> 
+                <div class="fg">   
+                    <label class="form_label" for="ubicacion">Ubicación:</label>
+                    <input class="controls" type="text" name="ubicacion" id="ubicacion" placeholder="Ubicación del dispositivo">
+                </div>
+                <div class="fg">   
+                    <label class="form_label" for="descripcion">Descripción:</label>
+                    <textarea class="controls" name="descripcion" id="descripcion" placeholder="Descripción"></textarea>
+                </div>
+                <div class="fg">   
+                    <label class="form_label" for="configuracion">Configuración:</label>
+                    <input class="controls" type="text" name="configuracion" id="configuracion" placeholder="Configuración">
+                </div>    
+                    <input class="botons" type="submit" value="Agregar">
+                    
+            </div>
+        </form>
+        
+    </section>
+</body>
+</html>
+</body>
+</html>
