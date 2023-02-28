35507946e3fa6f82190cf2d5cf9592553d2dd852
GabrielCiprianoLara <cilg011022@itsuruapan.edu.mx>
2023-02-28 12:40:09 -0600

    Base de datos actualizada con datos y dispositivos

--- clases/fallucapp(1).sql
+++ clases/fallucapp(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 27, 2023 at 04:12 PM
+-- Generation Time: Feb 27, 2023 at 06:20 PM
 -- Server version: 10.9.4-MariaDB
 -- PHP Version: 8.2.2
 
@@ -29,7 +29,7 @@ USE `fallucapp`;
 -- Table structure for table `dispositivos`
 --
 
-CREATE TABLE `dispositivos` (
+CREATE TABLE `dispositivos` (  
   `idDispositivo` int(11) NOT NULL,
   `nombre` varchar(45) NOT NULL,
   `ubicacion` varchar(45) NOT NULL,
@@ -43,17 +43,41 @@ CREATE TABLE `dispositivos` (
 --
 
 INSERT INTO `dispositivos` (`idDispositivo`, `nombre`, `ubicacion`, `descripcion`, `configuracion`, `idTipoDisp`) VALUES
-(3, 'Switch Cisco', 'México', 'Switch en Mexico', 'config.pdf', 1),
-(4, 'Otro switch', 'México', 'Uno más', 'conf-swmx.txt', 1),
-(5, 'Router Cisco', 'Dubai', 'Está chidp', 'rt-cd.txt', 2),
-(6, 'RouterMX', 'Mexico', 'Más chido que el otro', 'confrtmx.pdf', 2),
-(7, 'Rep', 'Mexico', 'Repetidor', 'rep-conf.pdf', 3);
+(1, 'Router Cisco', 'Edificio A - Site 1', 'Router principal de la red', 'configrouter1.pdf', 1),
+(2, 'Switch HP', 'Edificio B - Site 2', 'Switch de acceso en el piso 2', 'configswitch2.txt', 2),
+(3, 'Firewall Fortinet', 'Edificio C - Site 3', 'Firewall de borde para la red corporativa', 'configfw3.txt', 3),
+(4, 'Router Juniper', 'Edificio D - Site 4', 'Router de enlace de fibra óptica', 'configrouter4.xml', 1),
+(5, 'Switch Aruba', 'Edificio E - Site 5', 'Switch de distribución de red', 'configswitch5.xml', 2),
+(6, 'Firewall Palo Alto', 'Edificio F - Site 6', 'Firewall para segmento de servidores', 'configfw6.txt', 3),
+(7, 'Router MikroTik', 'Edificio G - Site 7', 'Router para red de oficinas remotas', 'configrouter7.xml', 1),
+(8, 'Switch D-Link', 'Edificio H - Site 8', 'Switch de acceso en el piso 3', 'configswitch8.txt', 2),
+(9, 'Firewall Check Point', 'Edificio I - Site 9', 'Firewall para segmento de almacenamiento', 'configfw9.xml', 3),
+(10, 'Router Huawei', 'Edificio J - Site 10', 'Router de enlace de fibra óptica', 'configrouter10.txt', 1),
+(11, 'Switch Netgear', 'Edificio K - Site 11', 'Switch de distribución de red', 'configswitch11.xml', 2),
+(12, 'Firewall SonicWall', 'Edificio L - Site 12', 'Firewall de borde para la red de invitados', 'configfw12.txt', 3),
+(13, 'Router Ubiquiti', 'Edificio M - Site 13', 'Router de enlace de radiofrecuencia', 'configrouter13.xml', 1),
+(14, 'Switch TP-Link', 'Edificio N - Site 14', 'Switch de acceso en el piso 1', 'configswitch14.txt', 2),
+(15, 'Firewall Cisco ASA', 'Edificio O - Site 15', 'Firewall para segmento de seguridad de red', 'configfw15.xml', 3),
+(16, 'Modem ADSL', 'Edificio A', 'Modem ADSL 2+', 'modemadsl.cfg', 4),
+(17, 'Bridge TP-Link', 'Edificio B', 'Bridge punto a punto', 'bridgetplink.cfg', 5),
+(18, 'Repetidor WiFi', 'Edificio C', 'Repetidor inalámbrico 300 Mbps', 'repetidorwifi300.cfg', 6),
+(19, 'Hub 3Com', 'Edificio D', 'Hub Ethernet 8 puertos', 'hub3com.cfg', 7),
+(20, 'Router MikroTik', 'Edificio E', 'Router WiFi 2.4 GHz', 'routermikrotik.cfg', 1),
+(21, 'Switch Cisco Catalyst', 'Edificio F', 'Switch 2960X 24 puertos', 'switchciscocat.cfg', 2),
+(22, 'Firewall Sophos', 'Edificio G', 'Firewall XG 85', 'firewallsophos.cfg', 3),
+(23, 'Bridge Ubiquiti', 'Edificio H', 'Bridge de largo alcance', 'bridgeubiquiti.cfg', 5),
+(24, 'Repetidor TP-Link', 'Edificio I', 'Repetidor inalámbrico 750 Mbps', 'repetidortplink.cfg', 6),
+(25, 'Hub D-Link', 'Edificio J', 'Hub Ethernet 16 puertos', 'hubdlink.cfg', 7);
 
 --
 -- Triggers `dispositivos`
 --
 DELIMITER $$
 CREATE TRIGGER `ACTUALIZAR_INVENTARIO` AFTER INSERT ON `dispositivos` FOR EACH ROW UPDATE `inventario` SET cantidad = cantidad + 1 WHERE idTipoDisp = new.idTipoDisp
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `DISMINUIR_INVENTARIO` AFTER DELETE ON `dispositivos` FOR EACH ROW UPDATE `inventario` SET cantidad = cantidad - 1
 $$
 DELIMITER ;
 
@@ -120,9 +144,13 @@ CREATE TABLE `tipos_dispositivos` (
 --
 
 INSERT INTO `tipos_dispositivos` (`idTipoDisp`, `tipo`) VALUES
-(1, 'Switch'),
-(2, 'Router'),
-(3, 'Repetidor');
+(1, 'Router'),
+(2, 'Switch'),
+(3, 'Firewall'),
+(4, 'Modem'),
+(5, 'Bridge'),
+(6, 'Repetidor'),
+(7, 'Hub');
 
 --
 -- Triggers `tipos_dispositivos`
