f7d4e2655cf344e534304d173b634d95c05efde4
GonzalezEnrique <goae011205@itsuruapan.edu.mx>
2023-03-05 14:55:15 -0600

    Actualización clase Inventario

--- clases/Inventario.php
+++ clases/Inventario.php
@@ -4,24 +4,6 @@ include 'Conexion.php';
 
 class Inventario {
 
-    private $id;
-    private $cantidad;
-    private $id_dispositivo;
-
-    // public function __construct($id, $cantidad, $id_dispositivo) {
-    //     $this->id = $id;
-    //     $this->cantidad = $cantidad;
-    //     $this->id_dispositivo = $id_dispositivo;
-    // }
-
-    public function get_cantidad() {
-        return $this->cantidad;
-    }
-
-    public function get_id_dispositivo() {
-        return $this->id_dispositivo;
-    }
-
     public function get_all_inventario($page = 1) {
         $conn = new ConexionBD();
         $sql = 'SELECT * FROM inventario';
