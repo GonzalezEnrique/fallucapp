42d3e0ed9ef10e9839075c4b4329cd793cc05197
AndreaMendezCaracoza <125303406+AndreaMendezCaracoza@users.noreply.github.com>
2023-03-04 23:09:39 -0600

    Add files via upload

---
+++ vistas/Fallucapp.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, user-scalable =no, initial-scale=1.0, maximum-scale=1.0,minimun-scale=1.0">  
+    <link rel="stylesheet" type="text/css" href="../recursos/css/hojaEstiloAndy.css">
+    <link rel="shortcut icon" href="../recursos/imagenes/logo.png" type="image/x-icon">
+    <title>Fallucapp</title>
+</head>
+</header>
+<header class="header">
+<a href="../vistas/Fallucapp.php">
+            <div class="logo"> 
+                <img id="prinicpal" name="prinicpal" src= "../recursos/imagenes/logo.png" width="90">
+            </div>
+        </a>
+
+            <h1>Fallucapp</h1> 
+
+</header >
+       
+           
+    <div class="contenedor">
+   
+         
+    
+             <div id="imgfalla" name="imgfalla">
+                    <a href="../vistas/Fallas.php"> <img src="../recursos/imagenes/falla.png" width="120" height="120"> </a>
+                    <a href="../vistas/Fallas.php"><h2>Fallas</h2></a>
+                </div>
+
+                <div id="img2dispositivos" name="img2dispositivos">
+                    <a href="../vistas/dispositivos.php"> <img src="../recursos/imagenes/cableEthernet.png" width="120" height="120"> </a>
+                    <a href="../vistas/dispositivos.php" > <h2>Dispositivos</h2></a>
+                </div>
+                
+                <div id="img3protocolos" name="img3protocolos">
+                    <a href="../vistas/protocolos.php">  <img src="../recursos/imagenes/carpetaa.png" width="120" height="120"> </a>
+                    <a href="../vistas/protocolos.php"><h2>Protocolos</h2></a>
+
+                </div>
+                </div>  
+                </div>
+
+</html>
